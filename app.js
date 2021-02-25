ba50b538ea4d4252b99e27d72a0f812a2d866634
Syed Shadab <syed.addan.shadab@gmail.com>
2021-02-25 14:48:36 -0500

    Changed behavior of nav toggler button to change image on click

--- app.js
+++ app.js
@@ -1,26 +1,48 @@
-// let bg = document.querySelector('#bg');
-//             window.addEventListener('scroll', function(){
-//                 let value = window.scrollY;
-//                 bg.style.backgroundSize = 100 + value*2 + "px";
-//             })
-
-
 class App {
     constructor() {
         
-        this.$landingBg = document.querySelector('#bg'); 
+        this.$landingBg = document.querySelector('#bg');
+        this.$navBtn = document.querySelector('#nav-btn'); 
+        this.$navBtnImg = document.querySelector('#nav-btn-img');
+        this.navImgIdentifier = 0; 
 
         this.addEventListeners();
+        this.handleNavBtnClick();
+        this.displaySrc();
+
     };
 
+    displaySrc () {
+        console.log(`NavBtnImg Src: ${this.$navBtnImg.src}`);
+    }
+
     addEventListeners() {
-            window.addEventListener('scroll', event => {
+            window.addEventListener('scroll', () => {
             let value = window.scrollY;
             this.$landingBg.style.backgroundSize = 100 + value*2 + "px";
         });
+
+            this.$navBtn.addEventListener('click', event => {
+                this.handleNavBtnClick(event); 
+            });
+
     };
 
+    handleNavBtnClick(event) {
+        const isNavBtnClicked = this.$navBtn.contains(event.target);
+        console.log(isNavBtnClicked);
+        if(isNavBtnClicked && this.navImgIdentifier % 2 == 0) {
+            this.$navBtnImg.src = './Images/up-arrow.svg';
+            this.navImgIdentifier++;
+        }
+        else if(isNavBtnClicked && this.navImgIdentifier % 2 == 1) {
+            this.$navBtnImg.src = './Images/down-arrow.svg'; 
+            this.navImgIdentifier++;
+        } 
+    };
 
 };
 
 new App();
+
+
